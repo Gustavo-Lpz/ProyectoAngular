b912d5effcdbf7ce31d3282f6f70b744a63d50ab
Gustavo Adolfo López Delgado <hust_mw2000@hotmail.com>
2025-06-22 11:13:49 -0600

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListarDoctoresComponent } from "./listar-doctores/listar-doctores.component";
-import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.component';
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ListarDoctoresComponent, AgregarEmpleadoComponent],
+  imports: [RouterOutlet,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.component';
+import { ListarDoctoresComponent } from './listar-doctores/listar-doctores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'listar', component: ListarDoctoresComponent},
+    {path: 'agregar', component: AgregarEmpleadoComponent},
+    {path: '', redirectTo: 'listar', pathMatch: 'full'},
+    {path: '**', redirectTo: 'listar'}
+];
